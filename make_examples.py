64d86b5ab37a9e9660c1c57101c03c9550295041
Daniel Nebdal <dnebdal@gmail.com>
2019-04-22 01:14:27 +0200

    Use a more appropriate (gamma) distribution for fake survival data

--- make_examples.py
+++ make_examples.py
@@ -7,13 +7,13 @@ low_span   = 0.2
 low_shift  = 0
 N_each     = 30
 
-surv_sd_frac = 0.2
-A_median_surv = 12
-B_median_surv = 24
-C_median_surv = 36
-A_event_frac  = 0.8
-B_event_frac  = 0.5
-C_event_frac  = 0.2
+surv_sd_frac = 0.75
+A_scale = 12
+B_scale = 24
+C_scale = 36
+A_event_frac  = 0.9
+B_event_frac  = 0.8
+C_event_frac  = 0.7
 
 outfile      = "static/expression_example.txt"
 outfile_surv = "static/survival_example.txt"
@@ -55,9 +55,9 @@ def make_sample(group):
         return(numpy.append(res1,res2))
     return None
 
-def make_surv(names, median, sd, event_frac):
+def make_surv(names, scale, event_frac):
     N = len(names)
-    followup = median + sd*numpy.random.standard_normal(N)
+    followup = numpy.random.gamma(2, scale=scale, size=N)
     event = numpy.random.choice(a=[0,1], size=N, p=[1-event_frac, event_frac], replace=True)
     return pandas.DataFrame({'sampleid':names, 'followup':followup, 'event':event})
 
@@ -74,18 +74,15 @@ res.to_csv(outfile, sep="\t", index_label = "genes", float_format="%.3f")
 # Survival data
 a = make_surv(
     names = ["sample%02d_%s" % (i+1,'a') for i in range(N_each)],
-    median = A_median_surv, 
-    sd = A_median_surv * surv_sd_frac,
+    scale = A_scale,
     event_frac = A_event_frac)
 b = make_surv(
     names = ["sample%02d_%s" % (i+1,'b') for i in range(N_each)],
-    median = B_median_surv, 
-    sd = B_median_surv * surv_sd_frac,
+    scale = B_scale,
     event_frac = B_event_frac)
 c = make_surv(
     names = ["sample%02d_%s" % (i+1,'c') for i in range(N_each)],
-    median = C_median_surv, 
-    sd = C_median_surv * surv_sd_frac,
+    scale = C_scale,
     event_frac = C_event_frac)
 
 surv = a.append(b).append(c)
