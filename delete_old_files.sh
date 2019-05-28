4c677a5e5146d650b49d8a11bf337856aa10ab7c
Daniel Nebdal <dnebdal@gmail.com>
2019-05-28 10:27:50 +0000

    Make delete_old work when called from an outside directory

--- delete_old_files.sh
+++ delete_old_files.sh
@@ -11,6 +11,8 @@
 # This can fail the other way (delete the file but not the entry), but that fixes itself
 # with nothing more than log warnings next time around.
 
+cd $(dirname "$0")
+
 echo "select f.filename from state s join files f on (s.id=f.id)  where julianday('now') - julianday(s.added) > 1;" | sqlite3 state.sqlite | xargs rm >> ~/delete.log 2>&1
 echo "delete from files  where files.id in (select id from state  where julianday('now') - julianday(added) > 1.01);" | sqlite3 state.sqlite >> ~/delete.log 2>&1
 echo "delete from state  where  where julianday('now') - julianday(added) > 1.011;" | sqlite3 state.sqlite >> ~/delete.log 2>&1
